49bfdd9abc9b3b8a46a433b7e39c533399efcaa0
TypeAnyName <alexgox11@gmail.com>
2022-12-07 09:30:48 +0600

    login

--- core/views.py
+++ core/views.py
@@ -17,7 +17,7 @@ class LoginView(generics.GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exeption=True)
+        serializer.is_valid(raise_exception=True)
         user = serializer.save()
         login(request=request, user=user)
         return Response(serializer.data)
