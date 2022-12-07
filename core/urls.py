7983772e6e6dde1c192a06c51db9bbbb17284c66
TypeAnyName <alexgox11@gmail.com>
2022-12-07 08:36:28 +0600

    login

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('signup/', views.RegistrationView.as_view(), name="signup")
+    path('signup', views.RegistrationView.as_view(), name="signup"),
+    path('login', views.LoginView.as_view(), name="login")
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login
 from rest_framework import generics
+from rest_framework.response import Response
 
-from core.serializers import RegistrationSerializer
+from core.serializers import RegistrationSerializer, LoginSerializer
 
 USER_MODEL = get_user_model()
 
@@ -9,3 +10,14 @@ USER_MODEL = get_user_model()
 class RegistrationView(generics.CreateAPIView):
     model = USER_MODEL
     serializer_class = RegistrationSerializer
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exeption=True)
+        user = serializer.save()
+        login(request=request, user=user)
+        return Response(serializer.data)
